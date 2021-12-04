1240295519703bf99181f3088728793f6f2c797a
miikisalminen <miika.salminen11@outlook.com>
2021-12-04 19:00:53 +0200

    Day 4 done properly

--- 2021/Day 4/day4_1.py
+++ 2021/Day 4/day4_1.py
@@ -1,66 +1,69 @@
 input_list = []
 drawn = []
-rows = []
-cols = []
+boards = []
 
-# what boards rows are a part of: //5
-# what boards cols are a part of: +100
+# Marks every boards num if they have it, then checks for winners.
+# If a winner is found, the winning board is returned
+# Otherwise return -1
+def check_boards(num):
 
-def check_rows(ball_num):
-    for i in range(len(rows)):
-        for j in range(len(rows[i])):
-            if rows[i][j] == ball_num:
-                rows[i][j] = "X"
-    if ["X","X","X","X","X"] in rows:
-        #print("WINNING ROW:")
-        #print(rows.index(["X","X","X","X","X"])//5)
-        return rows.index(["X","X","X","X","X"])
-    else:
-        return -1
+    # Write out marked number
+    for i in range(len(boards)):
+        for j in range(len(boards[i])):
+            for k in range(len(boards[i][j])):
+                if boards[i][j][k] == num:
+                    boards[i][j][k] = "X"
+    
+    # Check for completed rows
+    for i in range(len(boards)):
+        for j in range(len(boards[i])):
+            if ["X","X","X","X","X"] in boards[i]:
+                return boards[i]
 
-def check_cols(ball_num):
-    for i in range(len(cols)):
-        for j in range(len(cols[i])):
-            if cols[i][j] == ball_num:
-                cols[i][j] = "X"
-    if ["X","X","X","X","X"] in cols:
-        print("WINNING COLUMN:")
-        print(cols.index(["X","X","X","X","X"]))
-        return True
-    else:
-        return False
+    # Check for completed columns
+    for i in range(len(boards)):
+        for j in range(5):
+            if [row[j] for row in boards[i]] == ["X","X","X","X","X"]:
+                return boards[i]
+
+    # Return -1 if no board has yet won
+    return -1
+
+# Calculates board score of the winner
+def calc_score(board, last_drawn):
+    score = 0
+    for row in board:
+        for num in row:
+            if num != "X":
+                score = score + int(num)
+    
+    print("Part 1 score: {0}".format(score*int(last_drawn)))
 
 def main():
     f = open("input.txt", "r")
     for x in f:
         if x.strip() != "":
             input_list.append(x.strip())
-    
+
+    # Creating a stack of the drawing numbers    
     drawn = input_list[0].split(",")
 
-    for i in range(1, len(input_list)):
-        rows.append(input_list[i].split())
+    # Filling boards[]
     tmp = []
-    for i in range(5):
-        for j in range(len(rows)):
-            tmp.append(rows[j][i])
-            if len(tmp) == 5:
-                cols.append(tmp)
-                tmp = []
-                continue
-        
-
-    # Drawing all numbers
+    for i in range(1, len(input_list)):
+        tmp.append(input_list[i].split())
+        if len(tmp) == 5:
+            boards.append(tmp)
+            tmp = []
+    
+    # Drawing all numbers until a winner
     while(len(drawn) > 0):
         last_drawn = drawn[0]
-        winning_board = check_rows(last_drawn)
-        if winning_board != -1:
-            print("the winning board is #" + str(winning_board))
+        outcome = check_boards(last_drawn)
+        if outcome != -1:
+            calc_score(outcome, last_drawn)
             break
         drawn.pop(0)
-    for i in range(winning_board-5, winning_board+5):
-        print(rows[i])
-    print(last_drawn)
 
 if __name__ == "__main__":
     main()
--- 2021/Day 4/day4_2.py
+++ 2021/Day 4/day4_2.py
@@ -1,1 +1,70 @@
-## not implemented... yet.... maybe never
+input_list = []
+drawn = []
+boards = []
+# Array to keep track of order of winning bingo cards
+winners = []
+
+# Marks every boards num if they have it, then checks for winners.
+# If a winner is found, the winning board is returned
+# Otherwise return -1
+def check_boards(num):
+
+    # Write out marked number
+    for i in range(len(boards)):
+        for j in range(len(boards[i])):
+            for k in range(len(boards[i][j])):
+                if boards[i][j][k] == num and boards[i] not in winners:
+                    boards[i][j][k] = "X"
+    
+    # Check for completed rows (add the cards to winners)
+    for i in range(len(boards)):
+        for j in range(len(boards[i])):
+            if ["X","X","X","X","X"] in boards[i] and boards[i] not in winners:
+                winners.append(boards[i])
+
+    # Check for completed columns (add the cards to winners)
+    for i in range(len(boards)):
+        for j in range(5):
+            if [row[j] for row in boards[i]] == ["X","X","X","X","X"] and boards[i] not in winners:
+                winners.append(boards[i])
+        
+# Calculates score of board given
+def calc_score(board, last_drawn):
+    score = 0
+    for row in board:
+        for num in row:
+            if num != "X":
+                score = score + int(num)
+    
+    print("Part 2 score: {0}".format(score*int(last_drawn)))
+
+def main():
+    f = open("input.txt", "r")
+    for x in f:
+        if x.strip() != "":
+            input_list.append(x.strip())
+
+    # Creating a stack of the drawing numbers    
+    drawn = input_list[0].split(",")
+
+    # Filling boards[]
+    tmp = []
+    for i in range(1, len(input_list)):
+        tmp.append(input_list[i].split())
+        if len(tmp) == 5:
+            boards.append(tmp)
+            tmp = []
+   
+    # Drawing all numbers until every board has won
+    while(len(drawn) > 0):
+        last_drawn = drawn[0]
+        check_boards(last_drawn)
+        if len(winners) == len(boards):
+            break
+        drawn.pop(0)
+
+    # Calculate score of the last index in winners, i.e the bingo card that finishes last.
+    calc_score(winners[len(winners)-1], last_drawn)
+
+if __name__ == "__main__":
+    main()
